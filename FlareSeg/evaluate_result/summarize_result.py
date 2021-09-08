68913839c6b256ced88baedae71d70a92cbd1bc2
Shanghai-Aitrox-Technology <proxima@fosun.com>
2021-09-08 16:46:06 +0800

    add get_5fold_average_result

--- FlareSeg/evaluate_result/summarize_result.py
+++ FlareSeg/evaluate_result/summarize_result.py
@@ -21,6 +21,35 @@ def get_1fold_average_result(result_path):
             print('{}: {}'.format(name, np.mean(data)))
 
 
+def get_5fold_average_result(result_dir):
+    result_folder = os.listdir(result_dir)
+    all_result = []
+    column_header = ['Liver_DSC', 'Liver_NSC', 'Kidney_DSC', 'Kidney_NSC', 'Spleen_DSC', 'Spleen_NSC',
+                     'Pancreas_DSC', 'Pancreas_NSC', 'Average_DSC', 'Average_NSC']
+    for fold in result_folder:
+        result_path = result_dir + fold + '/ind_seg_result.csv'
+        data_df = load_df(result_path)
+        df_column_header = data_df.columns.values
+        fold_result = []
+        print('\n{}:'.format(fold))
+        for name in column_header:
+            if name in df_column_header:
+                data = data_df[name].values
+                fold_result.append(data)
+                print('mean {}: {}'.format(name, np.mean(data)))
+                print('std {}: {}'.format(name, np.std(data)))
+        all_result.append(fold_result)
+    all_result = np.array(all_result)
+    print('\naverage of 5-fold cross validation: ')
+    for idx, name in enumerate(column_header):
+        print('mean {}: {}'.format(name, np.mean(all_result[:, idx])))
+        print('std {}: {}'.format(name, np.std(all_result[:, idx])))
+
+
 if __name__ == '__main__':
-    result_path = '/fileser/zhangfan/efficientSegmentation/FlareSeg/evaluate_result/output/'
-    get_1fold_average_result(result_path)
+    # result_path = '/fileser/zhangfan/efficientSegmentation/FlareSeg/evaluate_result/output/'
+    # get_1fold_average_result(result_path)
+    result_dir = '/fileser/zhangfan/efficientSegmentation_refractor/FlareSeg/fine_efficient_seg/output_fold5_exp_0902/' \
+                 'test/experiment_2_EfficientSegNet_ResBaseConvBlock_AnisotropicConvBlock_AnisotropicAvgPooling_size-' \
+                 '192_channel-16_depth-4_loss-diceAndFocal_metric-dice/'
+    get_5fold_average_result(result_dir)
